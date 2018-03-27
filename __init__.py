916bf8aedfcef1ad9474fb58491b858996e174c0
jarbasai <jarbasai@mailfence.com>
2018-03-27 17:08:12 +0100

    dialog and settings meta

--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@ import cv2
 from os.path import dirname, exists, expanduser
 from os import makedirs
 from threading import Thread
+import json
+
 
 __author__ = 'jarbas'
 
@@ -102,6 +104,7 @@ class SharedVisionFeed(object):
 
 class WebcamSkill(MycroftSkill):
     def __init__(self):
+        self.create_settings_meta()
         super(WebcamSkill, self).__init__()
         platform = self.config_core.get("enclosure", {}) \
             .get("platform", "unknown")
@@ -110,6 +113,8 @@ class WebcamSkill(MycroftSkill):
 
         if "video_source" not in self.settings:
             self.settings["video_source"] = 0
+        if "play_sound" not in self.settings:
+            self.settings["play_sound"] = True
         if "picture_path" not in self.settings:
             self.settings["picture_path"] = expanduser("~/webcam")
         if "camera_sound_path" not in self.settings:
@@ -134,6 +139,75 @@ class WebcamSkill(MycroftSkill):
         self.shared_vision = VisionFeed()
         self.last_timestamp = time.time()
 
+    def create_settings_meta(self):
+        meta = {
+            "name": "Camera Skill",
+            "skillMetadata": {
+                "sections": [
+                    {
+                        "name": "video_source",
+                        "fields": [
+                            {
+                                "type": "label",
+                                "label": "video source for webcam, usually 0"
+                            },
+                            {
+                                "name": "video_source",
+                                "type": "number",
+                                "label": "video_source",
+                                "placeholder": "0",
+                                "value": "0"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "camera_sound_path",
+                        "fields": [
+                            {
+                                "type": "label",
+                                "label": "path to wav or mp3 sound file to play on picture taken"
+                            },
+                            {
+                                "name": "camera_sound_path",
+                                "type": "text",
+                                "label": "camera_sound_path",
+                                "placeholder": join(dirname(__file__),
+                                                      "camera.wav"),
+                                "value": join(dirname(__file__),
+                                                      "camera.wav")
+                            },
+                            {
+                                "type": "checkbox",
+                                "name": "play_sound",
+                                "label": "play sound on taking picture",
+                                "value": "true"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "picture_path",
+                        "fields": [
+                            {
+                                "type": "label",
+                                "label": "where to save taken pictures"
+                            },
+                            {
+                                "name": "picture_path",
+                                "type": "text",
+                                "label": "picture_path",
+                                "placeholder": expanduser("~/webcam"),
+                                "value": expanduser("~/webcam")
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        meta_path = join(dirname(__file__), 'settingsmeta.json')
+        if not exists(meta_path):
+            with open(meta_path, 'w') as fp:
+                json.dump(meta, fp)
+
     def initialize(self):
         self.vision_thread = Thread(target=self.read_frame)
         self.vision_thread.setDaemon(True)
@@ -148,7 +222,8 @@ class WebcamSkill(MycroftSkill):
 
     @intent_file_handler("take_picture.intent")
     def handle_take_picture(self, message):
-        if self.settings["camera_sound_path"]:
+        if exists(self.settings["camera_sound_path"]) and \
+                self.settings["play_sound"]:
             if ".wav" in self.settings["camera_sound_path"]:
                 play_wav(self.settings["camera_sound_path"])
             elif ".mp3" in self.settings["camera_sound_path"]:
@@ -156,6 +231,7 @@ class WebcamSkill(MycroftSkill):
 
         cv2.imwrite(join(self.settings["picture_path"], time.asctime() +
                          ".jpg"), self.feed)
+        self.speak_dialog("picture")
 
     def handle_get_picture(self, message):
         path = None
