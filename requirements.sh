edf519f56f8ddc444d6a6100570f4fcd3fda5084
JarbasAi <JARBASAI@mailfence.com>
2018-04-14 00:10:25 +0100

    openv venv

--- requirements.sh
+++ requirements.sh
@@ -1,27 +1,64 @@
 # The requirements.sh is an advanced mechanism and should rarely be needed.
 # Be aware that it won't run with root permissions and 'sudo' won't work
-# in most cases.
+# in most cases.  Py msm handles invokes the script with sudo internally if the platform allows it
 
 #detect distribution using lsb_release (may be replaced parsing /etc/*release)
 dist=$(lsb_release -d |awk '{print$2}')
+echo $dist
+dependencies=( python-opencv )
+
+# default pm
+pm="apt-get install -y"
 
 #setting dependencies and package manager in relation to the distribution
 if [ "$dist"  == "Arch"  ]; then
     pm="pacman -S"
-    dependencies=( python-opencv )
-elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ]; then
-    pm="apt install"
-    dependencies=( python-opencv )
-elif [ "$dist" == "Raspbian" ]; then
-    # Can't do 'sudo' with standard MSM install on a Mark 1 or Picroft,
-    # use pkcon instead which doesn't need sudo
-    pkcon install python-opencv
-    exit
+elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ] || [ "$dist" == "antiX" ]; then
+    pm="apt-get install -y"
+elif [ "$dist"  == "Raspbian"  ]; then
+    pm="pkcon install"
+elif [ "$dist"  == "Fedora" ] || [ "$dist" == "RedHat" ] || [ "$dist" == "CentOS" ]; then
+    dependencies=( opencv-python)
+    pm="yum -y install"
 fi
 
 
 # installing dependencies
 for dep in "${dependencies[@]}"
 do
-    sudo $pm $dep
+    echo "installing: $dep"
+    $pm $dep
 done
+
+
+# do extra stuff, like compiling from source`
+# exit 1 # indicate failure with any non 0 error code`
+
+# open cv https://medium.com/@manuganji/installation-of-opencv-numpy-scipy-inside-a-virtualenv-bf4d82220313
+
+user=$(sh -c 'echo $SUDO_USER')
+venv="mycroft"
+cp /usr/lib/python2.7/dist-packages/cv* /home/$user/.virtualenvs/$venv/lib/python2.7/site-packages/
+
+# compile dlib
+# installed in pip for now
+#rundir=$(pwd)
+#if ! git clone https://github.com/davisking/dlib ; then
+#  echo "Unable to clone Dlib!"
+#  exit 1
+#fi
+#cd dlib
+#if grep -q avx /proc/cpuinfo ; then
+#  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+#else
+#  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 ; cmake --build .
+#fi
+
+#if ! python ${rundir}/dlib/setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA ; then
+#  echo "Unable to install python Dlib!"
+#  exit 1
+#fi
+
+# exit with 0 to indicate success`
+
+exit 0
